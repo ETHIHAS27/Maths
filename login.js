ee80bd951db7514f5559d3c16fd7d050e5f6ec60
ETHIHAS27 <71704339+ETHIHAS27@users.noreply.github.com>
2020-11-15 13:45:07 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+player1_name = document.getElementById("player1_name").value;
+
+player2_name = document.getElementById("player2_name").value;
+
+function adduser(){
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+}
